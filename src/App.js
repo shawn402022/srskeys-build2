6791fd803b84aba401ef97b0f77c86c8dbee998e
Shawn <sgiovanni1@yahoo.com>
2023-03-03 02:40:20 -0500

    set up browser midi

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+
 function App() {
+
+  //```Checking to see if the browser accepts midi  ```
+  if(navigator.requestMIDIAccess){
+  navigator.requestMIDIAccess().then(success, failure)
+  }
+
+  function success(midiAccess) {
+    midiAccess.onstatechange = updateDevices
+
+    const inputs = midiAccess.inputs
+
+    inputs.forEach((input) => {
+      input.onmidimessage = handleInput
+    })
+  }
+
+  function handleInput(input) {
+    const command = input.data[0]
+    const note = input.data[1]
+    const velocity = input.data[2]
+
+
+    switch(command) {
+      case 144: //note on
+      if(velocity > 0) {
+        noteOn(note, velocity)
+      } else {
+        noteOff(note)
+      }
+      break
+      case 128: //note off
+      noteOff(note)
+      break
+    }
+  }
+  
+  function noteOn(note, velocity) {
+    console.log(note, velocity)
+  }
+
+  function noteOff(note) {
+    console.log(note)
+  }
+
+  function updateDevices(event) {
+    //console.log(`Name: ${event.port.name}, Brand: ${event.port.manufacturer}, State, ${event.port.state}, Type: ${event.port.type}` )
+  } 
+
+  function failure() {
+    console.log('could not connect')
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
